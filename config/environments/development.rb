22d5d864d52f2e094892561740fd78e7b8d7be73
Ramesh Indrajith <ramesh.kumar@snaile.com>
2023-01-27 09:34:35 +0530

    Fix Host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Additional comma-separated hosts for production.
+  config.hosts = [
+    ENV["RAILS_PRODUCTION_HOSTS"],
+    ENV["LB"]
+  ]
 end
